575ad0a274616b19d9be1f5be4bd0cdc11fdf3d9
Matheus Furtado <matheusfurtado2797@gmail.com>
2023-12-08 16:17:13 -0300

    primeira versão do projeto

---
+++ js/app.js
@@ -0,0 +1,40 @@
+function atualizarTextoPopup(texto) {
+    var textoPopup = document.querySelector('.msgConfirmacao');
+    textoPopup.textContent = texto;
+}
+
+function confirmarEntrega(imagem, botao) {
+    var confirmacao = document.getElementById("confirmacao");
+
+    confirmacao.style.display = "block";
+
+    var btnConfirmar = document.getElementById("btnConfirmar");
+    var btnCancelar = document.getElementById("btnCancelar");
+
+    btnConfirmar.addEventListener("click", function () {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        confirmacao.style.display = "none";
+    });
+
+    btnCancelar.addEventListener("click", function () {
+        confirmacao.style.display = "none";
+    });
+}
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name').textContent;
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        atualizarTextoPopup(`Deseja confirmar a entrega do jogo ${nomeJogo}?`);
+        confirmarEntrega(imagem, botao);
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
